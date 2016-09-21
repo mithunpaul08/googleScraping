50c04af0e2a939a44e1bb277fdf53debf6b20d91
mithun <mithunpaul08@gmail.com>
2016-09-20 21:25:42 -0700

    added select() in beautifulsoup

--- scraper.py
+++ scraper.py
@@ -3,6 +3,6 @@ res = requests.get('https://www.extension.iastate.edu/agdm/articles/edwards/EdwM
 res.raise_for_status()
 noStarchSoup = bs4.BeautifulSoup(res.text,"lxml")
 type(noStarchSoup)
-#for chunk in res.iter_content(100000):
-#    playFile.write(chunk)
-#playFile.close()
+elems = noStarchSoup.select('div')
+type(elems)
+len(elems)
