3ebf83ea5f63280fa60ac9ff90154f7e38f9f8a9
mithun <mithunpaul08@gmail.com>
2016-11-30 21:54:28 -0700

    changed to python2.7 syntax

--- main/src/scraper.py
+++ main/src/scraper.py
@@ -1,13 +1,3 @@
-#changes in this version.
-#todo
-#1.1 download and save html files from the first one results---done
-#1.2 download and save html files from the first ten results----done
-#1.3 download html files from the first ten results---done
-#2. convert first 10 html results to text and save them---done
-#3. download pdf files from the first ten results--done
-#4. convert the pdf files into txt and save them---done
-#5. create a folder structure
-
 import requests, bs4, sys, webbrowser, html2text, os , PyPDF2
 
 # encoding=utf8
@@ -40,7 +30,7 @@ for i in range(numOpen):
 
     #create a unique file name to store each of the results
     stubFilename='waterResults'
-    combinedFileName=stubFilename +i
+    combinedFileName=stubFilename +`i`
     waterResultFile = open(combinedFileName, 'wb+')
     for chunk in res.iter_content(100000):
         waterResultFile.write(chunk)
@@ -48,7 +38,7 @@ for i in range(numOpen):
     hrefValue=linkElems[i].get('href')
 
     if(hrefValue.find('.pdf')>0):
-        print("file number " + i+  " is a pdf file")
+        print 'file number ' + `i`+  ' is a pdf file'
         os.rename(combinedFileName,combinedFileName+'.pdf')
         pdfFileObj = open(combinedFileName+'.pdf', 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -71,7 +61,7 @@ for i in range(numOpen):
         target.close()
     else:
         #if file is html or txt
-        print("file number " + i+  " is not a pdf file")
+        print'file number ' + `i`+  ' is not a pdf file'
 
         #get the unicode converted file and rename it as html
         os.rename(combinedFileName,combinedFileName+'.html')
@@ -90,8 +80,9 @@ for i in range(numOpen):
     	#ignore links
         h = html2text.HTML2Text()
     	# Ignore converting links from HTML
-        h.ignore_links = True
-        convertedText=h.handle(myhtml)
+    	h.ignore_links = True
+    	#convert html to text
+    	convertedText=h.handle(myhtml)
 
         # #write the converted text to a txt file
         #target = open(outputDirectory+combinedFileName+'InTxtFormat.txt', 'w')
@@ -104,3 +95,11 @@ for i in range(numOpen):
 
 
 
+#todo
+#1.1 download and save html files from the first one results---done
+#1.2 download and save html files from the first ten results----done
+#1.3 download html files from the first ten results---done
+#2. convert first 10 html results to text and save them---done
+#3. download pdf files from the first ten results--done
+#4. convert the pdf files into txt and save them---done
+#5. create a folder structure
